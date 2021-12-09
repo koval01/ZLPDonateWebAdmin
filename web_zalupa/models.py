76d21c10602461e557730b74126d5ee7a5d1db3a
yaroslav.koval <ksvarog2004@gmail.com>
2021-12-09 19:11:34 +0200

    update

--- web_zalupa/models.py
+++ web_zalupa/models.py
@@ -27,7 +27,6 @@ class ServiceDonateStatus(models.Model):
     nameService = models.CharField('Услуга', max_length=255, default='Випка')
     namePlayer = models.CharField('Игрок', max_length=255, default='lomaka')
     statusPay = models.CharField('Статус', choices=STATUS_, default='wait', max_length=16)
-    price = models.IntegerField('Цена в рублях', max_length=255)
     serviceId = models.IntegerField('ID услуги', max_length=255)
 
     def __str__(self):
