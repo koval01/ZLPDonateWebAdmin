0255016f9f8ea68b6b159c5076f8e62ddf761609
yaroslav.koval <ksvarog2004@gmail.com>
2021-12-10 22:44:53 +0200

    update

--- django_web/urls.py
+++ django_web/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('web_zalupa.urls')),
+    path('', admin.site.urls),
+    path('/web', include('web_zalupa.urls')),
 ]
