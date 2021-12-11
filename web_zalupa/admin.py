a9b15a059495dfd0b2663820c59588fb0fb4f05b
yaroslav.koval <ksvarog2004@gmail.com>
2021-12-11 13:17:01 +0200

    update

--- web_zalupa/admin.py
+++ web_zalupa/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import ServiceDonate, ServiceDonateStatus
+from .models import ServiceDonate, ServiceDonateStatus, SystemSettings
 
 admin.site.register(ServiceDonate)
 admin.site.register(ServiceDonateStatus)
+admin.site.register(SystemSettings)
 admin.site.site_header = 'ЗаЛуПа'
